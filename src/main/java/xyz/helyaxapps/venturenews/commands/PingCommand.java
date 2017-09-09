9547f0fd9059829b57cd8a3b85e9e0b9a3856504
SkyriaS <phimalex94@gmail.com>
2017-09-09 18:14:32 +0200

    added a ping command to test

---
+++ src/main/java/xyz/helyaxapps/venturenews/commands/PingCommand.java
@@ -0,0 +1,30 @@
+package xyz.helyaxapps.venturenews.commands;
+
+
+import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.core.hooks.ListenerAdapter;
+
+
+
+public class PingCommand extends ListenerAdapter
+{
+
+
+    private long system_time;
+
+
+    @Override
+
+    public void onMessageReceived(MessageReceivedEvent event)
+    {
+        if (event.getMessage().getRawContent().equalsIgnoreCase("vn:ping"))
+        {
+            system_time = System.currentTimeMillis();
+            event.getChannel().sendMessage("Pong!").queue( message -> message.editMessageFormat(" :ping_pong:  Pong! ``%dms``", System.currentTimeMillis() - system_time).complete());
+        }
+    }
+
+
+
+
+}
