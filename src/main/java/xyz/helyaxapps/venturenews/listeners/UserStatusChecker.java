ca9f2e8efdbb7b85b0ec8cf7ffbbaae3a53a0bfc
SkyriaS <phimalex94@gmail.com>
2017-09-10 18:48:02 +0200

    nothing

--- src/main/java/xyz/helyaxapps/venturenews/listeners/UserStatusChecker.java
+++ src/main/java/xyz/helyaxapps/venturenews/listeners/UserStatusChecker.java
@@ -10,7 +10,7 @@ public class UserStatusChecker extends ListenerAdapter
     @Override
     public void onUserOnlineStatusUpdate(UserOnlineStatusUpdateEvent event) {
 
-        if (event.getUser().getId().equals("249225518077968384"))
+        if (event.getUser().getId().equals("249225518077968384") || event.getUser().getId().equals("276123714859565068"))
         {
             event.getGuild().getTextChannelById("356075131430240257").sendMessage("You went AFK!").queue();
         }
