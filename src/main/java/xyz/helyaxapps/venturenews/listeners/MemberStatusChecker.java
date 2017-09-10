49094cd0cf27de7a9e65f4eb54637119c3bec1db
RaptorDream <rdatchane.19@gmail.com>
2017-09-10 20:05:00 +0200

    added handling for Online status

--- src/main/java/xyz/helyaxapps/venturenews/listeners/MemberStatusChecker.java
+++ src/main/java/xyz/helyaxapps/venturenews/listeners/MemberStatusChecker.java
@@ -1,43 +1,28 @@
 package xyz.helyaxapps.venturenews.listeners;
 
 
-<<<<<<< HEAD
-import net.dv8tion.jda.*;
 import net.dv8tion.jda.core.OnlineStatus;
 import net.dv8tion.jda.core.events.user.UserOnlineStatusUpdateEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
 
-public class MemberStatusChecker extends ListenerAdapter
-{
+public class MemberStatusChecker extends ListenerAdapter {
 
 
     @Override
     public void onUserOnlineStatusUpdate(UserOnlineStatusUpdateEvent event) {
-        super.onUserOnlineStatusUpdate(event);
 
-       if(event.getGuild().getMemberById("249225518077968384").getOnlineStatus() == OnlineStatus.OFFLINE) {
-            event.getGuild().getTextChannelById("356075105509572608").sendMessage("You've gone offline!").queue();
-       }
-=======
-
-import java.util.EventListener;
-
-import net.dv8tion.jda.core.OnlineStatus;
-import net.dv8tion.jda.core.events.user.UserOnlineStatusUpdateEvent;
-
-
-public class MemberStatusChecker implements EventListener
-{
-
-
-    public void onUserOnlineStatusUpdate(UserOnlineStatusUpdateEvent event)
-    {
-
-        if (event.getGuild().getMemberById("276123714859565068").getOnlineStatus() == OnlineStatus.OFFLINE)
+        if (event.getGuild().getMemberById("249225518077968384").getOnlineStatus() == OnlineStatus.OFFLINE)
         {
-            event.getGuild().getTextChannelById("356075105509572608").sendMessage("oui");
+            event.getGuild().getTextChannelById("356075105509572608").sendMessage("You've gone offline!" + event.getUser().getAsMention()).queue();
+
+        } else if (event.getGuild().getMemberById("249225518077968384").getOnlineStatus() == OnlineStatus.ONLINE)
+
+        {
+            event.getGuild().getTextChannelById("356075105509572608").sendMessage("You've gone online" + event.getUser().getAsMention()).queue();
         }
 
->>>>>>> 4a5c922e4befd5e783b758a6004fd191d178cf91
+
     }
+
 }
+
