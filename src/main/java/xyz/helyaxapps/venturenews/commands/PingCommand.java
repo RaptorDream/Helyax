8a70989a0525fd9eaa81c13295f9789963867f26
RaptorDream <rdatchane.19@gmail.com>
2017-09-16 19:15:39 +0200

    Update PingCommand.java

--- src/main/java/xyz/helyaxapps/venturenews/commands/PingCommand.java
+++ src/main/java/xyz/helyaxapps/venturenews/commands/PingCommand.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 DATCHANAMOURTY Rohitkumar (rdatchane.19@gmail.com) & PHIMANESONE Alex
+ * Copyright 2017 DATCHANAMOURTY Rohitkumar (rdatchane.19@gmail.com) & PHIMANESONE Alex (phimalex94@gmail.com)
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
